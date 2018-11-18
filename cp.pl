2070be1c0d1f20ed752f59016ed0e3e833b069f7
cdanielgomes <cdanielgomes@github.com>
2018-11-18 02:49:32 +0000

    value_moves; possible steps for ai

--- cp.pl
+++ cp.pl
@@ -1,20 +1,47 @@
 :- use_module(library(random)).
-
+:- include('board.pl').
 
 
 
 randomMove(Type, Board, NewBoard):-
-	random(1, 20, R),
-	random(1, 20, C),
+	length(Board, N),
+	N1 is N +1,
+	random(1, N1, R),
+	random(1, N1, C),
 	insertOnPositon(R, C, Type, Board, NewBoard).
 
 
 randomMove(Type, Board, NewBoard):-
 	randomMove(Type, Board, NewBoard).
 
-	
+/*	
+getPlays(_, L, L, Plays, AuxPlay):- Plays = AuxPlay.
+
+getPlays(Board, Line, Column, Plays, AuxPlay):-
+	getElemInPosition(Board, Line, Column, Type), 
+	Type = 0,
+	append(Plays, [Line, Column], Play),
+	NL is Line +1, 		
+	getIncremental(Board, NL, Column, Play, AuxPlay).
+
+getPlays()
 
 
+incLine(Line, Column, NL, NC):-
+	NL is Line +1, Column = NC.
+*/
 
+%% a valid play is always valid doesnt matter who is playing
+valid_moves(Board, Player, ListOfMoves):-
+	findall([R,C],getElemInPosition(Board, R, C, 0), ListOfMoves). 
+
+possibleSequences(N, Sequence):- Sequence = [0, N, N, N, N].
+possibleSequences(N, Sequence):- Sequence = [N, N, N, N, 0]. 
+possibleSequences(N, Sequence):- Sequence = [0, N, N, N].
+possibleSequences(N, Sequence):- Sequence = [N, N, N, 0]. 
+possibleSequences(N, Sequence):- Sequence = [0, N, N].
+possibleSequences(N, Sequence):- Sequence = [N, N, 0]. 
+possibleSequences(N, Sequence):- Sequence = [0, N].
+possibleSequences(N, Sequence):- Sequence = [N, 0]. 
 
 %bestMove:-
