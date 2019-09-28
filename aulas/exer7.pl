b3cba5b02f8ca73c5cb75b8d2ce83ef4e0e61aed
Joao Alves <joao-carlos.alves@hotmail.com>
2019-09-28 17:43:10 +0100

    mini clean

--- aulas/exer7.pl
+++
@@ -1,9 +0,0 @@
-:-use_module(library(clpfd)).
-
-peru:- Vars=[A,B],
-		domain(Vars, 0, 9),
-		
-		A * 1000 #= S,
-		S + 600 + 70 + B #= N,
-		K * 72 #= N,
-		labeling([], Vars), write(Vars), nl, write(K).
