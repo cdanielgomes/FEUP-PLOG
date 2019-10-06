006648beb2ea7654f2d9bb093390dbd657c993c1
cdanielgomes <cdanielgomes@github.com>
2019-10-07 00:08:12 +0100

    nem eu sei o que fiz

--- Straight 4/logic.pl
+++ Straight 4/logic.pl
@@ -1,6 +1,6 @@
 :- include('display.pl').
 :- use_module(library(lists)).
-
+:- use_module(library(clpfd)).
 
 getPiece(Board, X, Y, Piece) :-
     nth0(X, Board, Elem),
@@ -12,7 +12,7 @@ setPiece(Board, 0, 0, Temp, NewBoard) :-
 
 
 setPiece([F|T], X, 0, Piece, Temp, NewBoard) :-
-    X \= 0, 
+    X\=0,
     setRow(F, X, Piece, [], Row),
     append(Temp, [Row], List),
     setPiece(T, 0, 0, List, NewBoard).
@@ -23,14 +23,48 @@ setPiece([F|T], X, Y, Piece, Temp, NewBoard) :-
     setPiece(T, X, Y1, Piece, List1AndList2, NewBoard).
 
 
-setRow([_|T], 0, Piece, TempRow, Row) :-
+setRow([F|T], 0, Piece, TempRow, Row) :-
+    F==0,
     append(TempRow, [Piece], Row1),
     append(Row1, T, Row).
+    
 
 setRow([F|T], X, Piece, TempRow, Row) :-
     append(TempRow, [F], List),
     X1 is X-1,
-    setRow(T, X1, Piece, List, Row). 
+    setRow(T, X1, Piece, List, Row).
+
+
+
+hor_win([B|_], Player) :-
+    checkHorizontal(B, Player).
+
+hor_win([_|T], Player) :-
+    hor_win(T, Player).
+
+checkHorizontal([X, X, X, X, _], X).
+checkHorizontal([_, X, X, X, X], X).
+
+
+vert_win([B|T], Player) :- transpose([B|T], [M|N]), hor_win([M|N], Player). %nao se vai poder usar mas para ja fica
+
+
+diagWin([B|T]).
+
+diagWinLR(Board, X, Y, Temp) :- nth0(Y, Board, Row), nth0(X, Row, Elem), X1 is X + 1, Y1 is Y + 1, append(Temp, [Elem], Temp2), diagWinLR(Board, X1, Y1, Temp2).
+
+diagWinLR(Board, X, Y, Temp) :- length(Temp, Int), Int >= 4.
+
+
+seq4(_, _, 4, P) :- write(P).
+
+seq4([F | T], F, Counter, _) :- F \= 0, Counter2 is Counter + 1, seq4(T, F, Counter2, F).  
+
+seq4([F| T], _, Counter, _) :- Counter \= 4, Counter2 is 1, seq4(T, F, Counter2, F).  
+
+
+
+
 
 % setPiece([ [0, 0, 0, 0, 0],
 %                        [0, 0, 0, 0, 0],
@@ -38,3 +72,4 @@ setRow([F|T], X, Piece, TempRow, Row) :-
 %                        [0, 0, 0, 0, 0],
 %                        [0, 0, 0, 0, 0]
 %                      ], 1 , 1, 1, [], N).
+% seq4([0,1,1,1,1,2,3,2,3],-1, 0, P).
