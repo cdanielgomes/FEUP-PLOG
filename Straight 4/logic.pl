9524d95669ce7b2552d01718702874381129bed4
cdanielgomes <cdanielgomes@github.com>
2019-10-12 19:49:02 +0100

    moving pieces

--- Straight 4/logic.pl
+++ Straight 4/logic.pl
@@ -11,7 +11,6 @@ setPiece(Board, 0, 0, Temp, NewBoard) :-
 
 
 setPiece([F|T], X, 0, Piece, Temp, NewBoard) :-
-    X\=0,
     setRow(F, X, Piece, [], Row),
     append(Temp, [Row], List),
     setPiece(T, 0, 0, List, NewBoard).
@@ -22,16 +21,15 @@ setPiece([F|T], X, Y, Piece, Temp, NewBoard) :-
     setPiece(T, X, Y1, Piece, List1AndList2, NewBoard).
 
 
-setRow([F|T], 0, Piece, TempRow, Row) :-
-    F==0,
+setRow([_|T], 0, Piece, TempRow, Row) :-
     append(TempRow, [Piece], Row1),
     append(Row1, T, Row).
-    
 
 setRow([F|T], X, Piece, TempRow, Row) :-
     append(TempRow, [F], List),
     X1 is X-1,
     setRow(T, X1, Piece, List, Row).
+
 
 
 %%%%% find a winner
@@ -205,6 +203,96 @@ check_seq4_left_right_down_up(Board, X, Y, _, Length, Player) :-
                                   0,
                                   Length,
                                   Player).  
+
+
+
+move(Board, Xi, Yi, Xf, Yf, Player, NewBoard) :-
+    nth0(Yi, Board, Row),
+    nth0(Xi, Row, Elemi),
+    Elemi=Player,
+    nth0(Yf, Board, Row2),
+    nth0(Xf, Row2, Elemf),
+    Elemf=0,
+    type_move(Board, Xi, Yi, Xf, Yf),
+    setPiece(Board, Xi, Yi, 0, [], Board1),
+    setPiece(Board1, Xf, Yf, Player, [], NewBoard).
+
+
+% move( [ [0, 0, 0, 0, 0],
+%         [0, 0, 0, 0, 0],
+%         [0, 0, 0, 0, 0],
+%         [0, 0, 0, 0, 0],
+%         [0, 0, 0, 0, 1]
+%       ], 4,4,0,0,1, B).
+
+type_move(Board, Xi, Yi, Xf, Yf) :-
+    VectorX is Xf-Xi,
+    VectorY is Yf-Yi,
+    can_move(Board,
+             VectorX,
+             VectorY,
+             Xi,
+             Yi,
+             Xf,
+             Yf).
+
+
+can_move(_, 0, 0, _, _, _, _, _) :-
+    fail.
+
+can_move(Board, 0, _, Xi, Yi, _, Yf) :-
+    move_vertically(Board, Xi, Yi, Yf).
+
+can_move(Board, _, 0, Xi, Yi, Xf, _) :-
+    move_horizontally(Board, Yi, Xi, Xf).
+
+can_move(Board, _, _, Xi, Yi, Xf, Yf) :-
+    Xf-Xi=:=Yf-Yi,
+    move_diagonally(Board, Xi, Yi, Xf, Yf).
+
+
+move_vertically(_, _, Yf, Yf).
+
+move_vertically(Board, X, Yi, Yf) :-
+    Sum is (Yf-Yi)div abs(Yf-Yi),
+    Y1 is Yi+Sum,
+    nth0(Y1, Board, Row),
+    nth0(X, Row, Elem),
+    Elem=0,
+    move_vertically(Board, X, Y1, Yf). 
+     
+
+move_horizontally(_, _, Xf, Xf).
+
+move_horizontally(Board, Y, Xi, Xf) :-
+    Sum is (Xf-Xi)div abs(Xf-Xi),
+    X1 is Xi+Sum,
+    nth0(X1, Board, Row),
+    nth0(Y, Row, Elem),
+    Elem=0,
+    move_horizontally(Board, Y, X1, Xf). 
+     
+
+
+% ends when Xi = Xf and Yf = Yi
+%move_diagonally(_, Xi, Xf, Yi, Yf) :- (Xi-Yi) = (Xf-Yf).
+move_diagonally(_, Xi, Yi, Xi, Yi).
+
+
+move_diagonally(Board, Xi, Yi, Xf, Yf) :-
+    SumX is (Xf-Xi)div abs(Xf-Xi),
+    SumY is (Yf-Yi)div abs(Yf-Yi),
+    X1 is Xi+SumX,
+    Y1 is Yi+SumY,
+    nth0(X1, Board, Row),
+    nth0(Y1, Row, Elem),
+    Elem=0,
+    move_diagonally(Board, X1, Y1, Xf, Yf). 
+     
+
+
+
+
 
  reload :-
     reconsult('logic.pl').
